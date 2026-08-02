8721cf9b7775b66403593a529bbd5b0335b07b3f
SiddhiiBabar <siddhibabar93@gmail.com>
2026-08-02 14:52:41 +0530

    added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send(' heyyyy this is my webhook....');
+  res.send(' Add webhook....');
 });
 
 app.listen(port, () => {
